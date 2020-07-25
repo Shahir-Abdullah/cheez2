f8d4caea503fdf69dbc44665f525c5224b631a1b
shahir-abdullah <shahir2561@gmail.com>
2020-07-25 11:34:06 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Router } from "@reach/router";
+import Home from "./pages/Home";
+import ResponsiveNavigation from "./components/ResponsiveNavigation";
+import Dashboard from "./pages/Dashboard";
+import DashboardSelector from "./pages/DashboardSelector";
+import "./assets/scss/base.scss";
+import "./App.css";
+import axios from "axios";
+if (process.env.NODE_ENV === "development") {
+  require("./assets/mock-endpoints/mocks");
+}
 
 function App() {
+  const navLinks = [
+    {
+      text: "Home",
+      path: "/",
+      icon: "ion-ios-home",
+    },
+    {
+      text: "Analytics",
+      path: "/analytics",
+      icon: "ion-ios-analytics",
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ResponsiveNavigation
+        navLinks={navLinks}
+        logo={logo}
+        background="#fff"
+        hoverBackground="#ddd"
+        linkColor="#777"
+      ></ResponsiveNavigation>
+      <Router>
+        <Home path="/" />
+        <Dashboard axios={axios} path="/analytics/:dashboardName" />
+        <DashboardSelector axios={axios} path="/analytics" />
+      </Router>
     </div>
   );
 }
